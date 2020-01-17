3f3792d7a74783d06a68cfdca9057326ed48721b
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-17 12:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const record = [
-<<<<<<< HEAD
-  { year: "2018", result: "L" },
-  { year: "2017", result: "W" },
-  { year: "2016", result: "N/A" }
-];
-function superbowlWin(arr) {
-  let findWin = arr.find(function(w) {
-    return w["result"] === "W";
-  });
-  if (findWin === undefined) {
-    return findWin;
-  } else {
-    return findWin["year"];
-  }
-}
-=======
+
   {year: "2018", result: "L"},
   {year: "2017", result: "W"},
   {year: "2016", result: "N/A"}
